1c0566c6e0d7aae39708270dd902b3dad0123454
sebastian navarro <sebastiannavarro.fl@gmail.com>
2022-10-19 16:11:46 +0200

    herokuTryp

--- play.rb
+++
@@ -1,8 +0,0 @@
-require 'open-uri'
-require 'json'
-
-url = "https://tmdb.lewagon.com/movie/top_rated"
-movies = JSON.parse(URI.open(url).read)["results"]
-movies.each do |movie|
-  p movie["original_title"]
-end
